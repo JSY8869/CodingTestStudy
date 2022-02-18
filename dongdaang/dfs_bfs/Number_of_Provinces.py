7ec56039b4b4dc43175163af70b93e726c1a7423
dongdaang <dongin6502@gmail.com>
2022-02-18 11:17:25 +0900

    Number_of_Provinces 리팩토링
    visited 리스트 내부 값을 boolean 형식으로 수정 완료 #10

--- dongdaang/dfs_bfs/Number_of_Provinces.py
+++ dongdaang/dfs_bfs/Number_of_Provinces.py
@@ -1,18 +1,18 @@
 class Solution:
     def findCircleNum(self, isConnected):
-        visited = [0] * len(isConnected)
+        visited = [False] * len(isConnected)
         
         def dfs(pos):
-            visited[pos] = 1
+            visited[pos] = True
             for i in range(len(isConnected[pos])):
-                if isConnected[pos][i] == 1 and visited[i] == 0:
+                if isConnected[pos][i] == 1 and visited[i] == False:
                     dfs(i)
                     
         result = 0
         
         for i in range(len(isConnected)):
             for j in range(len(isConnected)):
-                if isConnected[i][j] == 1 and visited[i] == 0:
+                if isConnected[i][j] == 1 and visited[i] == False:
                     dfs(i)
                     result += 1
         return result
